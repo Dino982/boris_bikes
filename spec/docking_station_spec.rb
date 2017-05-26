a9fd40347280ef30b0576d0a6d773c7525a6055c
Andrew Clarke <andrewclarke982@gmail.com>
2017-05-26 16:47:41 +0100

    Added exception /guard when trying to dock bike when capacity is full

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,22 +3,27 @@ require 'docking_station'
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 
-  # it 'releases a working bike' do
-  #   bike = subject.release_bike
-  #   expect(bike).to be_working
-  # end
-
   it { is_expected.to respond_to(:dock).with(1).argument }
 
   it { is_expected.to respond_to :bike }
 
-  it 'returns docked bikes' do
-    bike = Bike.new
-    subject.dock(bike)
-    expect(subject.bike).to eq bike
+  describe '#dock' do
+
+    it 'returns docked bikes' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.bike).to eq bike
+    end
+
+    it 'raises an error when docking station is full' do
+      subject.dock(Bike.new)
+      expect { subject.dock(Bike.new) }.to raise_error "Docking station full"
+    end
   end
 
+
   describe '#release_bike' do
+
     it 'release a bike' do
       bike = Bike.new
       subject.dock(bike)

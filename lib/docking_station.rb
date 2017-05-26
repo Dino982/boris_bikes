ec015d1be272347ddf0d96dce20aa830dbec47a5
Andrew Clarke <andrewclarke982@gmail.com>
2017-05-26 16:31:14 +0100

    Added exception /guard when trying to release bike when there is no bike

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,11 +1,12 @@
 require_relative 'bike'
 
 class DockingStation
-  
+
   attr_reader :bike
 
-  def release_bike
-    Bike.new
+  def release_bike #nb methods can release other objects...like a new instance of bike
+    fail 'No bikes available' unless @bike #guard condition
+    @bike
   end
 
   def dock(bike)
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,10 +3,10 @@ require 'docking_station'
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 
-  it 'releases a working bike' do
-    bike = subject.release_bike
-    expect(bike).to be_working
-  end
+  # it 'releases a working bike' do
+  #   bike = subject.release_bike
+  #   expect(bike).to be_working
+  # end
 
   it { is_expected.to respond_to(:dock).with(1).argument }
 
@@ -16,5 +16,17 @@ describe DockingStation do
     bike = Bike.new
     subject.dock(bike)
     expect(subject.bike).to eq bike
+  end
+
+  describe '#release_bike' do
+    it 'release a bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end
+
+    it 'raises an error when no bike available' do
+      expect { subject.release_bike}.to raise_error 'No bikes available'
+    end
   end
 end
